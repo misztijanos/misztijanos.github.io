c17dbe518d2da339690307052e8f5f1cf2694e4c
misztijanos <misztijanos@yahoo.com>
2018-10-18 18:41:09 +0300

    moved the new site up front, added all the links

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Miszti János' Portfolio</title>
+	<link href="https://fonts.googleapis.com/css?family=Lato:400,700&amp;subset=latin-ext" rel="stylesheet"> 
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<header>
+		<!--  -->
+		<div class="name-container">
+			<a href="index.html"><img class="logo" src="images/logo.png"></img></a>
+			<div class="name">Miszti János</div>
+		</div>
+		<ul class="navigation">
+			<li><a class="not-last" href="index.html">Home</a></li>
+			<li><a class="not-last" href="about.html">About</a></li>
+			<li><a class="not-last" href="index.html#port-container">Portfolio</a></li>
+			<li><a class="not-last" href="index.html#testimonials">Testimonials</a></li>
+			<li><a class="not-last" href="#">Blog</a></li>
+			<li><a class="last" href="#">Hire János</a></li>
+		</ul>
+	</header>
+	<div class="banner-container">
+		<section class="who-am-i">
+			<h2>I'm János, a front-end web developer ... who might design his own stuff from time to time.</h2>
+			<div class="red-line"></div>
+			<p> Scroll down to take a look at my portfolio, and let's create something beautiful together.</p>
+		</section>
+		<img class="face" src="images/face.png" alt="face"></img>
+	</div>
+
+	<section class="port-container" id="port-container">
+				<a href="http://www.sonialiao.com/" class="port-item sonia">
+			<img src="images/port/sonia-logo.png" alt="Sonia Liao"></img>
+			<p class="slide-up">HTML5, CSS3, Responsive, Javascript</p>
+		</a>
+		<a href="croatia-about.html" class="port-item croatia">
+			<img src="images/port/croatia-logo.png" alt="Vancouver Pet Sitting"></img>
+			<p class="slide-up">HTML5, CSS3, Responsive, WordPress, Javascript</p>
+		</a>
+		<a href="famtrav-about.html" class="port-item famtrav">
+			<img src="images/port/famtrav-logo.png" alt="Family Travel Adventure"></img>
+			<p class="slide-up">WordPress, SQL, PHP, HTML5, CSS3, Javascript</p>
+		</a>
+				<a href="comic-about.html" class="port-item comic">
+			<img src="images/port/comic-logo2.png" alt="Vancouver Pet Sitting"></img>
+			<p class="slide-up">HTML5, CSS3, Responsive, Javascript</p>
+		</a>
+		<a href="janos-about.html" class="port-item janos">
+			<img src="images/port/janos-logo.png" alt="Miszti Janos Homepage"></img>
+			<p class="slide-up">HTML5, CSS3, Photoshop, Responsive, no-script</p>
+		</a>
+		<a href="vps-about.html" class="port-item vps">
+		<!--vps is a unique class, to allow us to pick the right background color in css -->
+			<img src="images/port/vps-logo.png" alt="Vancouver Pet Sitting"></img>
+			<p class="slide-up">JQuery, Bootstrap, PHP, HTML5, CSS3</p>
+		</a>
+	</section>
+	
+	<section class="featured">
+		<h1>How I can help</h1>
+		<p>A front-end developer's role is to combine design and business logic to achieve a user-facing product. This takes equal parts technology and vision. Front-end developers are the bridge between the designer and the back-end programmer, which means they have to be both creative and tech-savvy. With my experience I can:</p>
+    <h3>Create responsive websites that work on a wide range of devices</h3>
+    <h3>Assist with the production of clean and functional design</h3>
+    <h3>Bridge the communication gap between designers and developers</h3>
+    <h3>Integrate your website into an easy to use content management system</h3>
+	</section>
+
+	<section class="testimonials" id="testimonials">    
+    <h1>Testimonials</h1>
+    <blockquote>
+    	<p>James was extremely professional. He gave us regular updates and delivered work as promised and went beyond in ensuring a smooth transition of the website to us. We will definitely be contacting him if we need any website development done!.</p>
+    	<footer>Bryan, <a href="https://www.freelancer.com/u/CobyAirport">CobyAirport</a></footer>
+    </blockquote>
+    <blockquote>
+    	<p>Good freelancer A+++. I hope to work with him another time.</p>
+    	<footer>Luca Fierro, <a href="https://www.netcom.it/">NetCom</a></footer>
+    </blockquote>
+    <blockquote>
+    	<p>Working with János was great. He is an incredibly talented developer, but what really makes him stand out is his work ethic and steady approach. Time after time, and without us asking, he added enhancements and improvements that resulted in a better end product for us and our clients.</p>
+    	<footer>Jonathan Ellison, <a href="https://wirebox.co.uk/">WireBox</a></footer>
+    </blockquote>
+	</section>
+
+	<section class="footer">Copyright © 2018 <a href="index.html">Miszti János</a></section>
+</body>
+</html>

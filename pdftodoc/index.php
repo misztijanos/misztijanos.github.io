0d087624bb7b9d2c398604d082b25c1af6d82bc6
misztijanos <misztijanos@yahoo.com>
2018-11-14 19:44:41 +0200

    pdf2doc partial

--- pdftodoc/index.php
+++ pdftodoc/index.php
@@ -3,7 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title>PdftoDoc</title>
-	<link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+	
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script src="my-scripts.js"></script>
 </head>
 <body>
 	<header>
@@ -28,34 +33,99 @@ ensuring better quality than many other converters.
 	</header>
 
 	<div class="main-content">
-    <div class= "buttons-wrapper">
-        <button class="upload-button"><img class="upload-icon" src="images/upload-icon.png"></img> Upload Files</button>
-        <button class="clear-button"><img class="clear-icon" src="images/clear-icon.png"></img> Clear Queue</button>
-    </div>
-    
-    <div id="carousel-wrapper">
-        <div id="carousel-prev-wrapper">
-            <div id="carousel-prev" class="disabled ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only ui-button-disabled ui-state-disabled" role="button" title=""><span class="ui-button-icon-primary ui-icon ui-icon-triangle-1-w"></span><span class="ui-button-text"></span></div>
-        </div> 
-        <div id="container" style="width: 885px; left: 22px;">
-            <div id="carousel">
-                <ul id="filelist" class="plupload_filelist"></ul>
-            <div id="plupload_drop">Drop Your Files Here</div></div>
+        <div class="reclama-doi">
+            <p>your ad goes here</p>
         </div>
-        <div id="carousel-next-wrapper">
-            <div id="carousel-next" class="disabled ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only ui-button-disabled ui-state-disabled" role="button" title=""><span class="ui-button-icon-primary ui-icon ui-icon-triangle-1-e"></span><span class="ui-button-text"></span></div>
+        <div class= "buttons-wrapper">
+            <button class="upload-button"><img class="upload-icon" src="images/upload-icon.png"></img> Upload Files</button>
+            <button class="clear-button"><img class="clear-icon" src="images/clear-icon.png"></img> Clear Queue</button>
         </div>
-    </div>
-		<div class= "buttons-wrapper">
-    	<button class="download-button"><img class="download-icon" src="images/download-icon.png"></img> Download All</button>
-    </div>
+        
+        <!-- Bootstrap Carousel: -->
+        <div id="imageCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
+          <ol class="carousel-indicators">
+            <li data-target="#imageCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#imageCarousel" data-slide-to="1"></li>
+            <li data-target="#imageCarousel" data-slide-to="2"></li>
+          </ol>
+             <div class="carousel-inner">
+               <div class="item active">
+                 <div class="row">
+                   <div class="col-md-4">
+                        <div class="elem-wrap">
+                            <div class="elem"></div>
+                        </div> 
+                   <!-- 
+                     <img src="https://1.bp.blogspot.com/-E8GdZQshD7I/WI1-yGMXseI/AAAAAAAACfs/ZndWplkyLpgVPO5IQ294pjxr9rq8YtqcwCLcB/s640/boot1.png" class="img-responsive" /> -->
+                   </div>
+                   <div class="col-md-4">
+                   <!-- 
+                     <img src="https://1.bp.blogspot.com/-E8GdZQshD7I/WI1-yGMXseI/AAAAAAAACfs/ZndWplkyLpgVPO5IQ294pjxr9rq8YtqcwCLcB/s640/boot1.png" class="img-responsive" /> -->
+                   </div>
+                   <div class="col-md-4">
+                   <!-- 
+                     <img src="https://1.bp.blogspot.com/-E8GdZQshD7I/WI1-yGMXseI/AAAAAAAACfs/ZndWplkyLpgVPO5IQ294pjxr9rq8YtqcwCLcB/s640/boot1.png" class="img-responsive" /> -->
+                   </div>
+                   
+                 </div>
+               </div>
+               <div class="item">
+                 <div class="row">
+                   <div class="col-md-4"><!-- 
+                     <img src="https://2.bp.blogspot.com/-hmUAa7DH5N4/WI2KJqMdhrI/AAAAAAAACf8/huv-RB55G_QpCI-9G-iA0JrS1pqCswEogCLcB/s640/boot2.png" class="img-responsive" /> -->
+                   </div>
+                   <div class="col-md-4"><!-- 
+                     <img src="https://2.bp.blogspot.com/-ZWboxYj_gz0/WInKMMQA_HI/AAAAAAAACe4/4ihHdvifuKYJNQElLULtivsCne9ZDbM3ACLcB/s640/di.png" class="img-responsive" /> -->
+                   </div>
+                   <div class="col-md-4"><!-- 
+                     <img src="https://1.bp.blogspot.com/-E8GdZQshD7I/WI1-yGMXseI/AAAAAAAACfs/ZndWplkyLpgVPO5IQ294pjxr9rq8YtqcwCLcB/s640/boot1.png" class="img-responsive" /> -->
+                   </div>
+                 </div>
+               </div>
+               <div class="item">
+                 <div class="row">
+                   <div class="col-md-4"><!-- 
+                     <img src="https://1.bp.blogspot.com/-E8GdZQshD7I/WI1-yGMXseI/AAAAAAAACfs/ZndWplkyLpgVPO5IQ294pjxr9rq8YtqcwCLcB/s640/boot2.png" class="img-responsive" /> -->
+                   </div>
+                   <div class="col-md-4"><!-- 
+                     <img src="https://1.bp.blogspot.com/-E8GdZQshD7I/WI1-yGMXseI/AAAAAAAACfs/ZndWplkyLpgVPO5IQ294pjxr9rq8YtqcwCLcB/s640/boot1.png" class="img-responsive" /> -->
+                   </div>
+                   <div class="col-md-4"><!-- 
+                     <img src="https://2.bp.blogspot.com/-ZWboxYj_gz0/WInKMMQA_HI/AAAAAAAACe4/4ihHdvifuKYJNQElLULtivsCne9ZDbM3ACLcB/s640/di.png" class="img-responsive" /> -->
+                   </div>
+                 </div>
+               </div>
+              </div>
+              <a class="left carousel-control" href="#imageCarousel" role="button" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#imageCarousel" role="button" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+              </a>
+         </div>
 
-    <div class="ha-wrapper">
-        <div class="hl">
+		<div class= "buttons-wrapper">
+    	   <button class="download-button"><img class="download-icon" src="images/download-icon.png"></img> Download All</button>
         </div>
-    </div>
+        <div class="reclama-trei">
+            <p>your ad goes here</p>
+        </div>
+
+        <div class="ha-wrapper">
+            <div class="hl">
+            </div>
+        </div>
 
 	</div>
+    <footer>
+        <div id="bottom">
+        <div id="like-it">Like it? Share it!</div>
+        <div id="share" class="share" data-url="https://pdf2doc.com/" data-title="Convert PDF to Word Online"><a href="/" title="Please share this page via twitter" class="pop share-bw share-bw-twitter" style="display: inline-block;"></a><a href="/" title="Please share this page via facebook" class="pop share-bw share-bw-facebook" style="display: inline-block;"></a><a href="/" title="Please share this page via googleplus" class="pop share-bw share-bw-googleplus" style="display: inline-block;"></a><a href="/" title="Please share this page via stumbleupon" class="pop share-bw share-bw-stumbleupon" style="display: inline-block;"></a><a href="/" title="Please share this page via reddit" class="pop share-bw share-bw-reddit" style="display: inline-block;"></a><div style="clear: both;"></div></div>
+        <p id="footer-privacy">All data submitted/uploaded is deleted after 1 hour.</p>
+    </div>
+    </footer>
 
 </body>
 </html>

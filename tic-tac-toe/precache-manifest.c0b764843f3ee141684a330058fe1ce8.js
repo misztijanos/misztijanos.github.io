ed9283fbc1ecdceb26045840aff751d9191a8c7c
misztijanos <misztijanos@yahoo.com>
2020-05-01 14:25:35 +0300

    added max-width 1390

---
+++ tic-tac-toe/precache-manifest.c0b764843f3ee141684a330058fe1ce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9fcb5ce3847b8f1766f7c81dd21f63",
+    "url": "./index.html"
+  },
+  {
+    "revision": "35d071fd016e494fddf7",
+    "url": "./static/css/main.223931d2.chunk.css"
+  },
+  {
+    "revision": "2c48cd995de152bccd1a",
+    "url": "./static/js/2.1c0e8306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1c0e8306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d071fd016e494fddf7",
+    "url": "./static/js/main.4240f47e.chunk.js"
+  },
+  {
+    "revision": "cd13de6a19e74233017e",
+    "url": "./static/js/runtime-main.634a3763.js"
+  }
+]);
---
+++ tic-tac-toe/precache-manifest.d9898070a15d958e639edd3029021fe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571819fd90d78c4518b0adcbcf0ed762",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3c24659a9bd9c2f2791",
+    "url": "/static/css/main.223931d2.chunk.css"
+  },
+  {
+    "revision": "2c48cd995de152bccd1a",
+    "url": "/static/js/2.1c0e8306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1c0e8306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c24659a9bd9c2f2791",
+    "url": "/static/js/main.203a5455.chunk.js"
+  },
+  {
+    "revision": "271da6c74b3b85186610",
+    "url": "/static/js/runtime-main.43639289.js"
+  }
+]);
